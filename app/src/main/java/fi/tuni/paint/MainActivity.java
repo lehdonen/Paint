5c142b0683011e0b5cedd4b8f3f957332d507e7b
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-03-24 21:57:32 +0200

    Clear menu functionality

--- app/src/main/java/fi/tuni/paint/MainActivity.java
+++ app/src/main/java/fi/tuni/paint/MainActivity.java
@@ -2,6 +2,9 @@ package fi.tuni.paint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case (R.id.clearCanvas) :
+                MyPaintView mpw = findViewById(R.id.mpw);
+                mpw.clear();
+                return true;
+        }
+        return false;
     }
 }
