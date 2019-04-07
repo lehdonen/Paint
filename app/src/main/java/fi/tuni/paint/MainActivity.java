bf5ad4192e399f5b623760867cda8687c70c9aaf
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-04-07 22:53:01 +0300

    Update MainActivity.java
    Add Menufunctions

--- app/src/main/java/fi/tuni/paint/MainActivity.java
+++ app/src/main/java/fi/tuni/paint/MainActivity.java
@@ -1,12 +1,18 @@
 package fi.tuni.paint;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +35,107 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
+        final MyPaintView mpw;
+        final MyColorDialog colorDialog;
 
         switch (item.getItemId()) {
             case (R.id.clearCanvas) :
-                MyPaintView mpw = findViewById(R.id.mpw);
+                mpw = findViewById(R.id.mpw);
                 mpw.clear();
+                return true;
+            case (R.id.chooseColor) :
+                mpw = findViewById(R.id.mpw);
+                colorDialog = new MyColorDialog(this, new MyColorDialog.OnColorChangedListener() {
+                    @Override
+                    public void OnColorChanged(int color) {
+                        mpw.setColor(color);
+                    }
+                }, mpw.getColor());
+                colorDialog.show();
+
+                return true;
+            case (R.id.drawMode) :
+                mpw = findViewById(R.id.mpw);
+
+                dialog = new Dialog(this);
+                dialog.setContentView(R.layout.draw_mode_dialog);
+                dialog.setTitle("Set Drawing Mode");
+                dialog.setCancelable(true);
+                dialog.show();
+
+                SeekBar sizeBar = dialog.findViewById(R.id.sizeBar);
+                sizeBar.setProgress((int) mpw.getBrushSize());
+                sizeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        mpw.setBrushSize(progress);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+
+                SeekBar objectSizeBar = dialog.findViewById(R.id.objectSizeBar);
+                objectSizeBar.setProgress((int) mpw.getObjectSize());
+                objectSizeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        mpw.setObjectSize(progress);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+
+                CheckBox fillBox = dialog.findViewById(R.id.fillBox);
+                fillBox.setChecked(mpw.isFill());
+                fillBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            mpw.setFill(true);
+                        } else {
+                            mpw.setFill(false);
+                        }
+                    }
+                });
+
+                return true;
+            case (R.id.eraser) :
+                mpw = findViewById(R.id.mpw);
+                mpw.setDrawMode(0);
                 return true;
         }
         return false;
+    }
+
+    public void onDrawModeButtonClick(View v) {
+        MyPaintView mpw = findViewById(R.id.mpw);
+
+        switch (v.getId()) {
+            case (R.id.b1) :
+                mpw.setDrawMode(1);
+                break;
+            case (R.id.b2) :
+                mpw.setDrawMode(2);
+                break;
+            case (R.id.b3) :
+                mpw.setDrawMode(3);
+                break;
+        }
     }
 }
