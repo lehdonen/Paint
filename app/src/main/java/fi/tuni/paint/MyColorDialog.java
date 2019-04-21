e425ae7f408463afb2c4cfbd792353765a6e8ed2
lehdonen <36726382+lehdonen@users.noreply.github.com>
2019-04-21 17:55:18 +0300

    Add Javadoc + dialog mode functionality

--- app/src/main/java/fi/tuni/paint/MyColorDialog.java
+++ app/src/main/java/fi/tuni/paint/MyColorDialog.java
@@ -7,32 +7,75 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
+/**
+ * Custom dialog for color changing.
+ */
 public class MyColorDialog extends Dialog {
+
+    /**
+     * Integer values for red, green and blue.
+     */
     private int r, g, b;
+
+    /**
+     * Integer value for initial color.
+     */
     private int initialColor;
+
+    /**
+     * Integer value for new color.
+     */
     private int colorToBe;
+
+    /**
+     * Helper integer to decide dialog layout.
+     */
+    private int dialogMode;
+
+    /**
+     * Color changed listener.
+     */
     private OnColorChangedListener listener;
 
 
+    /**
+     * Public interface for listening color changes.
+     */
     public interface OnColorChangedListener {
         void OnColorChanged(int color);
         void SaveRGB(int r, int g, int b);
     }
 
-    public MyColorDialog(Context context, OnColorChangedListener onColorChangedListener, int initialColor, int initialR, int initialG, int initialB) {
+    /**
+     *
+     * @param context Used to input context.
+     * @param onColorChangedListener Used to input color changed listener.
+     * @param initialColor Used to input initial color.
+     * @param initialR Used to input initial red value.
+     * @param initialG Used to input initial green value.
+     * @param initialB Used to input initial blue value.
+     * @param dialogMode Used to input dialog content view mode.
+     */
+    public MyColorDialog(Context context, OnColorChangedListener onColorChangedListener, int initialColor, int initialR, int initialG, int initialB, int dialogMode) {
         super(context);
         this.initialColor = initialColor;
         this.listener = onColorChangedListener;
         this.r = initialR;
         this.g = initialG;
         this.b = initialB;
+        this.dialogMode = dialogMode;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.color_picker_dialog);
+        if (dialogMode == 0) {
+            setContentView(R.layout.color_picker_dialog);
+        } else {
+            setContentView(R.layout.bg_color_dialog);
+        }
+
         setTitle("Pick a Color");
         setCancelable(true);
 
@@ -101,39 +144,81 @@ public class MyColorDialog extends Dialog {
         });
     }
 
+    /**
+     * Method for setting new red value.
+     *
+     * @param r Used to input new red value.
+     */
     public void setR(int r) {
         this.r = r;
         setColorToBe(r, g, b);
     }
 
+    /**
+     * Method for setting new green value.
+     *
+     * @param g Used to input new green value.
+     */
     public void setG(int g) {
         this.g = g;
         setColorToBe(r, g, b);
     }
 
+    /**
+     * Method for setting new blue value.
+     *
+     * @param b Used to input new blue value.
+     */
     public void setB(int b) {
         this.b = b;
         setColorToBe(r, g, b);
     }
 
+    /**
+     * Method for getting current red value.
+     *
+     * @return Current red value.
+     */
     public int getR() {
         return r;
     }
 
+    /**
+     * Method for getting current green value.
+     *
+     * @return Current green value.
+     */
     public int getG() {
         return g;
     }
 
+    /**
+     * Method for getting current blue value.
+     *
+     * @return Current blue value.
+     */
     public int getB() {
         return b;
     }
 
+    /**
+     * Method for setting new color preview.
+     *
+     * @param r Used to input new red value.
+     * @param g Used to input new green value.
+     * @param b Used to input new blue value.
+     */
     public void setColorToBe(int r, int g, int b) {
         this.colorToBe = Color.rgb(r, g, b);
 
         listener.OnColorChanged(getColorToBe());
     }
 
+    /**
+     * Method for getting new color.
+     *
+     * @return Integer value of new color.
+     */
     public int getColorToBe() {
         return this.colorToBe;
     }
